f995adb21aa01272844b3a597631d0bbf3fd474b
KangBaekGwa <ksu9801@gmail.com>
2025-08-18 11:16:40 +0900

    fix: ApiError status 및 code 추가

--- types/api.types.ts
+++ types/api.types.ts
@@ -6,9 +6,13 @@ export interface ApiResponse<T> {
 }
 
 export class ApiError extends Error {
+  public status: number;
+  public code?: string;
+
   constructor(status: number, message: string, code?: string) {
     super(message);
     this.name = 'ApiError';
+    this.status = status;
     this.code = code;
   }
 }
