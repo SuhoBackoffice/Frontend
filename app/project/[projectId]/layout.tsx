b658fff616339923529cbc5c37597e094c3f2eea
KangBaekGwa <ksu9801@gmail.com>
2025-10-01 16:51:04 +0900

    style: Project Nav 바 sticky 위치 수정

--- app/project/[projectId]/layout.tsx
+++ app/project/[projectId]/layout.tsx
@@ -17,10 +17,9 @@ export default async function ProjectLayout({
     <div className="min-h-dvh">
       {/* 좌측 사이드바 + 우측 본문 그리드 */}
       <div className="container mx-auto p-4 md:p-8 lg:grid lg:grid-cols-[240px_1fr] lg:gap-6">
-        <aside className="mb-6 lg:sticky lg:top-0 lg:mb-0 lg:self-start">
+        <aside className="mb-6 lg:sticky lg:top-[calc(var(--sticky-top))] lg:mb-0 lg:max-h-[calc(100dvh-var(--header-height)-24px)] lg:self-start lg:overflow-y-auto">
           <ProjectSidenav projectId={id} />
         </aside>
-
         <main className="space-y-6">{children}</main>
       </div>
     </div>
